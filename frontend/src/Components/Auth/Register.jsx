00a8aa29340f5f29a10af91ddb1bc77073ee2b1f
jishnu79 <jishnu@toolfe.com>
2024-07-05 22:36:52 +0530

    link

--- frontend/src/Components/Auth/Register.jsx
+++ frontend/src/Components/Auth/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from 'axios'
-const API = axios.create({baseURL:"http://127.0.0.1:5000"})
+const API = axios.create({baseURL:"https://social-media-api-orcin.vercel.app"})
 
 function Register() {
   const [error, setError] = useState(null);
