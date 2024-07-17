18c9fe3e910bf37f75b20f24565788dc9ddaf6fc
jishnu79 <jishnu@toolfe.com>
2024-07-17 12:17:58 +0530

    prisma connection

---
+++ users/src/interactors/userIntractor.ts
@@ -0,0 +1,8 @@
+import { IUserInterface } from "interfaces/userIntractor";
+
+export class ProductIntractor implements IUserInterface{
+    createUser(input: any) {
+        throw new Error("Method not implemented.");
+    }
+    
+}
---
+++ users/src/interfaces/userIntractor.ts
@@ -0,0 +1,3 @@
+export interface IUserInterface {
+    createUser (input:any):any
+}
