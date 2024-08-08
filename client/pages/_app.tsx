d6e32a4d8b367a220c10ccd4de27bd6a18d988d5
jishnu79 <jishnu@toolfe.com>
2024-08-08 14:36:12 +0530

    tr

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,8 @@
 import Laout from "@/Components/Laout";
-import EditModel from "@/Components/Modals/EditModel";
 import LoginModal from "@/Components/Modals/LoginModal";
 import RegisterModal from "@/Components/Modals/RegisterModal";
 import { store } from "@/Redux/Store";
-import "@/styles/globals.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
