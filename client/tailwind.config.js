a17bcbc8a5b57e5c0d99011fb707aaf01815519f
jishnu79 <jishnu@toolfe.com>
2024-08-08 15:49:42 +0530

    kl

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+      extend: {},
   },
-  plugins: []
+  variants: {
+      extend: {},
+  },
+  plugins: [],
 }
